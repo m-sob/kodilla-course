866740fd45b6853cd3559162eab19e50795d72ef
m-sob <m.sobianowska@gmail.com>
2020-10-15 13:27:55 +0200

    Mod. 6.2 Zad. pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        Calculator calculator = new Calculator();
+
+        //add
+        int sum = calculator.add(2,3);
+        if (sum == 5) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Error in add test!");
+        }
+
+        //subtract
+        int difference = calculator.subtract(5,4);
+        if (difference == 1) {
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Error in subtract test!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int a, b;
+
+    public static int add(int a, int b){
+        return (a + b);
+    }
+
+    public static int subtract(int a, int b){
+        return (a - b);
+    }
+
+
+}

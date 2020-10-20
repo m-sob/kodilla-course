36daa81872d0a4885e29a9d54b4957d8dfd4251d
m-sob <m.sobianowska@gmail.com>
2020-10-20 23:26:47 +0200

    0

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector {
+    public List<String> shapeCollector = new ArrayList<>();
+
+    public void addFigure(Shape shape) {
+        //  shapeCollector.add(Shape shape);
+    }
+
+    public void removeFigure(Shape shape) {
+        //  shapeCollector.remove(Shape shape);
+    }
+
+    public void getFigure(int n) {
+        //  shapeCollector.getFigure(int n);
+    }
+
+    public void showFigures(){
+        //  zwracającą nazwy wszystkich figur w postaci jednego Stringa.
+        //  shapeCollector.showFigures();
+    }
+}

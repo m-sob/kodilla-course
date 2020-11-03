51cf7df44817e39f249f2505c21fce6397e370e8
m-sob <m.sobianowska@gmail.com>
2020-11-03 10:37:42 +0100

    TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,7 @@
 package com.kodilla.testing;
-import com.kodilla.testing.user.SimpleUser;
-
-import com.kodilla.testing.calculator.Calculator;
+//import com.kodilla.testing.user.SimpleUser;
+//
+//import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
